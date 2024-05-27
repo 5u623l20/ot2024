071ab2d9e2611a321f6c3c37c622f67bc533d267
Muhammad Moinur Rahman <moin@bofh.im>
2024-05-27 07:32:53 +0200

    Add anti-debug methods

--- foo/main.c
+++ foo/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ptrace.h>
+
+// Anti-debugging function
+void anti_debug() {
+    if (ptrace(PTRACE_TRACEME, 0, 1, 0) == -1) {
+        printf("Debugger detected!\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 void foo(int *arr, int size) {
     for (int i = 0; i < size; i++) {
@@ -8,6 +17,7 @@ void foo(int *arr, int size) {
 }
 
 int main() {
+    anti_debug();
     int size = 10;
     int *arr = (int *)malloc(size * sizeof(int));
     for (int i = 0; i < size; i++) {
